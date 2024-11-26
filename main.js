a4572fa00eb2a3644330f66dfbdacbb7d6aa5936
Debiddo <dzheng@terpmail.umd.edu>
2024-11-26 01:24:05 -0500

    created the game section

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-import './sass/main.css';
+// import './sass/main.css';
+
+import gamesData from "./data/games.json" assert { type: "json" };
+import { getImageUrl } from "./utils";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // game grid
+  const gamesGrid = document.querySelector(".games__grid");
+  gamesData.forEach((game) => {
+    const gameCard = document.createElement("div");
+    gameCard.classList.add("game-card");
+
+    gameCard.innerHTML = `
+        <img class="game-card__image" src="${getImageUrl(
+          game.imageSrc
+        )}" alt="${game.title}">
+        <div class="game-card__title">${game.title}</div>
+        <div class="game-card__description">${game.description}</div>
+        <button class="game-card__link--demo btn"> <a href="${
+          game.demo
+        }" target="_blank">Live</a></button>
+        <button class="game-card__link--source btn"> <a href="${
+          game.source
+        }" target="_blank">Source</a></button>
+      `;
+
+    gamesGrid.appendChild(gameCard);
+  });
+});
